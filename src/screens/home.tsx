a63a658a6255183ff2ac967d4c17a0f951f9c7c3
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-11-29 17:50:26 -0300

    add group list

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,36 @@
+import { Group } from '@components/group'
 import { HomeHeader } from '@components/home-header'
 import { VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={item === groupSelected}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 32,
+        }}
+        style={{
+          marginVertical: 40,
+          maxHeight: 44,
+          minHeight: 44,
+        }}
+      />
     </VStack>
   )
 }
